cdd6e644695d5d51c10467185e9c68167116dd84
TwintailsIndustries <andre.eggmann@web.de>
2020-06-26 15:32:56 +0200

    bla

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-$('.checkbox').click(function() {
+$(document).on('change', '.checkbox', function() {
     alert("hello world");
     if(this.checked.class == "lorem-ipsum") {
         $(".hex.lorem-ipsum").toggle(this.checked);
