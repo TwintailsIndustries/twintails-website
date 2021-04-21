51b564f7694aaf67e61bb8d2b465d6d9b9f0079d
TwintailsIndustries <andre.eggmann@web.de>
2021-04-21 14:19:57 +0200

    new public

---
+++ public/js/script.js
@@ -0,0 +1,72 @@
+/*$(document).on('change', '.checkbox.lorem-ipsum', function() {
+    $(".hex.lorem-ipsum").fadeToggle( 400, "swing" );
+    $("h1").fadeToggle( 400, "swing" );
+});
+
+$(document).on('change', '.checkbox.daughters-nemesis', function() {
+    $(".hex.daughters-nemesis").fadeToggle( 400, "swing" );
+});*/
+
+/* Fixes height for propaganda-image-container */
+
+$( window ).on( "load", function() {
+    if($('gallery-content.is-visible')) {
+        var propagandaheight = $('.is-visible').css('height');
+        $("#fullimagecontainer").height(propagandaheight);
+    }
+    var test = '1000px';
+    var propagandaheight = $('.propaganda-gallery-container .propaganda-gallery [type="radio"]:checked ~ label ~ .gallery-content').css('height');
+    $("#fullimagecontainer").height(propagandaheight);
+});
+
+/*
+$('input[type=radio][name="student[course][disability]"]').change(function () {  
+$(document).on('change', 'js-gallery-tab', function() {*/
+$(document).on('input[type=radio][name="gallery-group"]').change(function () {
+    var propagandaheight = $('.propaganda-gallery-container .propaganda-gallery [type="radio"]:checked ~ label ~ .gallery-content').css('height');
+    $("#fullimagecontainer").height(propagandaheight);
+});
+
+ $(document).on('change', '.checkbox.nemesis-anti', function() {
+    if ($(".hex.nemesis-anti").hasClass('hide-hex')) {
+        // $(".hex.lorem-ipsum").height($(".hex-lorem-ipsum").find('.hook').height());
+        $(".hex.nemesis-anti").removeClass('hide-hex');
+    } else {
+        // $(".hex.lorem-ipsum").height(''); 
+        $(".hex.nemesis-anti").addClass('hide-hex');
+    }
+});
+
+$(document).on('change', '.checkbox.nemesis-pro', function() {
+    if ($(".hex.nemesis-pro").hasClass('hide-hex')) {
+        // $(".hex.daughters-nemesis").height($(".hex-daughters-nemesis").find('.hook').height());
+        $(".hex.nemesis-pro").removeClass('hide-hex');
+    } else {
+        // $(".hex.daughters-nemesis").height(''); 
+        $(".hex.nemesis-pro").addClass('hide-hex');
+    }
+});
+
+$(document).on('change', '.checkbox.threed-art', function() {
+    if ($(".hex.threed-art").hasClass('hide-hex')) {
+        $(".hex.threed-art").removeClass('hide-hex');
+    } else {
+        $(".hex.threed-art").addClass('hide-hex');
+    }
+});
+
+$(document).on('change', '.checkbox.character-design', function() {
+    if ($(".hex.character-design").hasClass('hide-hex')) {
+        $(".hex.character-design").removeClass('hide-hex');
+    } else {
+        $(".hex.character-design").addClass('hide-hex');
+    }
+});
+
+$(document).on('change', '.checkbox.illustration', function() {
+    if ($(".hex.illustration").hasClass('hide-hex')) {
+        $(".hex.illustration").removeClass('hide-hex');
+    } else {
+        $(".hex.illustration").addClass('hide-hex');
+    }
+});
